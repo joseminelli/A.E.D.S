3559d847d3eeada75d58dc69c96e8b16347a924b
José Minelli <zecarmineli@gmail.com>
2023-09-07 11:18:28 -0300

    cor no texto

--- C/Conversor HEX - RGB/conversor.c
+++ C/Conversor HEX - RGB/conversor.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Encerrar() {
+void Encerrar()
+{
     printf("\nPressione ENTER para continuar...");
     char x = '0';
-    do { x = getchar( ); } while ( '\n' != x );
+    do
+    {
+        x = getchar();
+    } while ('\n' != x);
     getchar();
 }
 
-void HextoRgb() {
-    char hexColor[7];  
+void HextoRgb()
+{
+    char hexColor[7];
     int vermelho, verde, azul;
 
     printf("\nDigite o codigo hexadecimal da cor (RRGGBB): ");
     scanf("%6s", hexColor);
 
     // sscanf converte o valor hexadecimal em decimal
-    if (sscanf(hexColor, "%2x%2x%2x", &vermelho, &verde, &azul) == 3) {
-        if (vermelho >= 0 && vermelho <= 255 && verde >= 0 && verde <= 255 && azul >= 0 && azul <= 255) {
-            printf("\nCor RGB: (%d, %d, %d)\n", vermelho, verde, azul);
-        } else {
+    if (sscanf(hexColor, "%2x%2x%2x", &vermelho, &verde, &azul) == 3)
+    {
+        if (vermelho >= 0 && vermelho <= 255 && verde >= 0 && verde <= 255 && azul >= 0 && azul <= 255)
+        {
+            printf("\nCor RGB: \x1b[38;2;%d;%d;%dm(%d , %d, %d)\x1b[0m\n", vermelho, verde, azul, vermelho, verde, azul);
+        }
+        else
+        {
             printf("Valores de cor fora do intervalo.\n");
         }
-    } else {
+    }
+    else
+    {
         printf("Entrada invalida. Forneca um codigo hexadecimal valido (RRGGBB).\n");
     }
 
@@ -30,7 +41,8 @@ void HextoRgb() {
     return 0;
 }
 
-void RgbtoHex(){
+void RgbtoHex()
+{
     int vermelho, verde, azul;
     char hexColor[7];
 
@@ -42,10 +54,13 @@ void RgbtoHex(){
     printf("Azul: ");
     scanf("%d", &azul);
 
-    if (vermelho >= 0 && vermelho <= 255 && verde >= 0 && verde <= 255 && azul >= 0 && azul <= 255) {
+    if (vermelho >= 0 && vermelho <= 255 && verde >= 0 && verde <= 255 && azul >= 0 && azul <= 255)
+    {
         sprintf(hexColor, "%02x%02x%02x", vermelho, verde, azul);
-        printf("\nCor HEX: %s\n", hexColor);
-    } else {
+        printf("\nCor HEX: \x1b[38;2;%d;%d;%dm%s\x1b[0m\n", vermelho, verde, azul, hexColor);
+    }
+    else
+    {
         printf("Valores de cor fora do intervalo.\n");
     }
 
@@ -53,31 +68,32 @@ void RgbtoHex(){
     return 0;
 }
 
-int main() {
+int main()
+{
     int opcao;
     do
     {
-    printf("\n=========Conversor de cores =========\n");
-    printf("1 - HEX -> RGB\n");
-    printf("2 - RGB -> HEX\n");
-    printf("0 - Sair\n");
-    printf("\nEscolha uma opcao: ");
-    scanf(" %d", &opcao);
+        printf("\n=========Conversor de cores =========\n");
+        printf("1 - HEX -> RGB\n");
+        printf("2 - RGB -> HEX\n");
+        printf("0 - Sair\n");
+        printf("\nEscolha uma opcao: ");
+        scanf(" %d", &opcao);
 
-    switch (opcao)
-    {
-        case 0: 
-        Encerrar();
-        exit(0);
-    case 1:
-        HextoRgb();
-        break;
-    case 2: 
-        RgbtoHex();
-        break;
-    default:
-        printf("Opcao invalida.\n");
-    }
+        switch (opcao)
+        {
+        case 0:
+            Encerrar();
+            exit(0);
+        case 1:
+            HextoRgb();
+            break;
+        case 2:
+            RgbtoHex();
+            break;
+        default:
+            printf("Opcao invalida.\n");
+        }
     } while (opcao != 0);
     return 0;
 }
